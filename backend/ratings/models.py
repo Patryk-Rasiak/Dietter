ea6f797d7e0a4e8d7a16e64859fced1fad4e8591
Patryk Rasiak <patryk01r@gmail.com>
2023-01-28 10:59:08 +0100

    Adding ratings

---
+++ backend/ratings/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from meals.models import Meal
+from diets.models import Diet
+
+
+class Rating(models.Model):
+    RATING_CHOICES = [(x/2, x/2) for x in range(0, 11)]
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.DecimalField(choices=RATING_CHOICES)
+    description = models.TextField(blank=True, null=True)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='ratings', null=True, blank=True)
+    diet = models.ForeignKey(Diet, on_delete=models.CASCADE, related_name='ratings', null=True, blank=True)
